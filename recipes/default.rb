28c7fecd0a412323a63daa322666a6330de2dcbb
Adnan Husain <adnan.panalytics@gmail.com>
2016-05-06 11:48:20 +0200

    Logging added

--- recipes/default.rb
+++ recipes/default.rb
@@ -106,9 +106,13 @@ ruby_block "get disks from shell output" do
         command = "for file in $(df | grep -E '^/dev' | awk -F '%' '{print $2}'); do file=`echo --diskpath=$file`; files=`echo $files $file`; done;  echo $files;"
         command_out = shell_out(command)
         node.set[:cw_mon][:options] = command_out.stdout
+        log "Command output: " + command_out
     end
     action :create
 end
+
+log "Optins changed"
+log node[:cw_mon][:options]
 
 
 options = ['--from-cron'] + node[:cw_mon][:options]
